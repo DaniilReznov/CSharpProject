1ab50a2809cac8cbd2ee7a7ee402cd13f94d4757
DaniilReznov <73659966+DaniilReznov@users.noreply.github.com>
2023-02-11 20:11:58 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Threading;
+using static WindowsFormsApp1.XMLParse;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form 
+    {
+        public DataTable dt = new DataTable();
+        public int number;
+        public bool visible = false;
+
+        public Form1() {
+            InitializeComponent();
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; // автоматически настраивается ширина столбцов
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                while (dt.Columns.Count != 0) {
+                    dt.Columns.Remove(dt.Columns[dt.Columns.Count - 1]);
+                }
+                FillData();
+            }
+            catch (Exception) {
+                MessageBox.Show("Выберите файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Выбор файла для парсинга
+        private void button2_Click(object sender, EventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog {
+                Filter = "xml files (*.xml)|*.xml",
+                RestoreDirectory = true
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                string filename = openFileDialog.FileName;
+                fileXML = File.ReadAllText(filename);
+            }
+        }
+
+        // Метод для создания строк в таблице
+        private void AddRows() {
+            XmlDocument docXML = new XmlDocument(); // XML-документ
+            docXML.LoadXml(fileXML); // загрузить XML
+            textBox7.Text = docXML.GetElementsByTagName("read")[0].InnerText;
+            number = Convert.ToInt32(textBox7.Text);
+            for (int i = 0; i < number; i++) {
+                dt.Rows.Add(new Object[] { "" });
+            }
+        }
+
+        // Метод для заполнения таблицы
+        private void FillData() {
+            ProductData2 productData2 = new ProductData2();
+            XMLParse parser = new XMLParse();
+            
+            // Создание колон в таблице
+            dt.Columns.Add("Код с криптохвостом");
+            dt.Columns.Add("Код без криптохвоста");
+            dt.Columns.Add("Дата производства");
+            dt.Columns.Add("Тип производства");
+            dt.Columns.Add("Тип сертификата");
+
+            AddRows();
+
+            productData2 = parser.XMLParse_Doc();
+
+            textBox1.Text = productData2.product_date1;
+            textBox2.Text = productData2.batch;
+            textBox3.Text = productData2.line;
+            textBox4.Text = productData2.startmarking;
+            textBox5.Text = productData2.stopmarking;
+            textBox6.Text = productData2.produced;
+            textBox7.Text = productData2.read;
+            textBox8.Text = productData2.record;
+            textBox9.Text = productData2.rejected;
+            textBox10.Text = productData2.production_order;
+
+            number = Convert.ToInt32(textBox7.Text);
+            for (int i = 0; i < number; i++) {
+                dt.Rows[i]["Код с криптохвостом"] = productData2.products.ElementAt(i).ki;
+                dt.Rows[i]["Код без криптохвоста"] = productData2.products.ElementAt(i).cis;
+                dt.Rows[i]["Дата производства"] = productData2.products.ElementAt(i).product_date;
+                dt.Rows[i]["Тип производства"] = productData2.products.ElementAt(i).tnved_code;
+                dt.Rows[i]["Тип сертификата"] = productData2.products.ElementAt(i).certificate_type;
+            }
+            //Присвоение DataTable для компонента DataGridView
+            dataGridView1.DataSource = dt;
+            //Сброс источника данных
+            dataGridView1.DataSource = null;
+            //Присвоение источника данных
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/XMLParse.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WindowsFormsApp1 {
+    internal class XMLParse {
+        public static string fileXML;
+        public struct ProductData {
+            // Низ
+            public string ki,
+                cis,
+                product_date,
+                certificate_type;
+            public int tnved_code;
+        };
+
+        public struct ProductData2 {
+            // Верх
+            public string product_date1,
+                batch,
+                line,
+                startmarking,
+                stopmarking,
+                produced,
+                read,
+                record,
+                rejected,
+                production_order;
+            public List<ProductData> products;
+        };
+
+        public ProductData2 XMLParse_Doc() {
+            XmlDocument docXML = new XmlDocument(); // XML-документ
+            docXML.LoadXml(fileXML); // загрузить XML
+            XmlElement xmlElement = docXML.DocumentElement;
+            ProductData2 productData2 = new ProductData2();
+            productData2.products = new List<ProductData>();
+            productData2.products.Clear();
+            ProductData productData = new ProductData();
+
+            foreach (XmlNode node in xmlElement.ChildNodes) {
+                if (node.Name == "product_date") productData2.product_date1 = node.InnerText;
+                if (node.Name == "batch") productData2.batch = node.InnerText;
+                if (node.Name == "line") productData2.line = node.InnerText;
+                if (node.Name == "startmarking") productData2.startmarking = node.InnerText;
+                if (node.Name == "stopmarking") productData2.stopmarking = node.InnerText;
+                if (node.Name == "produced") productData2.produced = node.InnerText;
+                if (node.Name == "read") productData2.read = node.InnerText;
+                if (node.Name == "record") productData2.record = node.InnerText;
+                if (node.Name == "rejected") productData2.rejected = node.InnerText;
+                if (node.Name == "production_order") productData2.production_order = node.InnerText;
+                if (node.Name == "products_list") {
+                    foreach (XmlNode node2 in node.ChildNodes) {
+                        if (node2.Name == "product") {
+                            foreach (XmlNode node3 in node2.ChildNodes) {
+                                if (node3.Name == "ki") {
+                                    productData.ki = node3.InnerText;
+                                }
+                                if (node3.Name == "cis") {
+                                    productData.cis = node3.InnerText;
+                                }
+                                if (node3.Name == "product_date") {
+                                    productData.product_date = node3.InnerText;
+                                }
+                                if (node3.Name == "tnved_code") {
+                                    productData.tnved_code = Convert.ToInt32(node3.InnerText);
+                                }
+                                if (node3.Name == "certificate_type") {
+                                    productData.certificate_type = node3.InnerText;
+                                }
+                            }
+                            productData2.products.Add(productData);
+                        }
+                    }
+                }
+            }
+            productData2.products.Add(productData);
+            return productData2;
+        }
+    }
+}
